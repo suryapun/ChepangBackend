a29f1d73bf17482d07a451910f46ed3dd890a217
SuryaPun <suryapundrums@gmail.com>
2022-01-28 09:19:48 -0600

    Initial commit

---
+++ src/main/java/com/grind/chepangbackend/ChepangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grind.chepangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChepangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChepangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grind/chepangbackend/ChepangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grind.chepangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChepangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
